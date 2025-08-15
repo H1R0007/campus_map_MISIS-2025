2bff8581a347933c9afebc54ebc858a679cdb821
Davidex01 <davidex010506@gmail.com>
2025-08-15 23:56:08 +0300

    class path_finder update

--- src/path_finder/path_finder.hpp
+++ src/path_finder/path_finder.hpp
@@ -11,5 +11,17 @@
 struct Point {
 	int point_id;
 	float point_length;
+
+
+	// Конструктор по умолчанию
+	Point() = default;
+
+	// Конструктор через параметры
+	Point(int _point_id, float _point_length) : point_id(_point_id), point_length(_point_length) {}
+
+	// Оператор сравнения для очереди по приоритету
 	bool operator>(const Point& other) const { return point_length > other.point_length; }
+
+	// Деструктор
+	~Point() = default;
 };
