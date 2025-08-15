20b9ddc53e20815e99097f45a6bf0ee6fa2422c5
Davidex01 <davidex010506@gmail.com>
2025-08-15 23:55:44 +0300

    class Node update

--- src/read_nodes/nodes.hpp
+++ src/read_nodes/nodes.hpp
@@ -4,13 +4,33 @@
 
 
 // Структура параметров точек
-struct Nod {
-	int nod_id;
-	std::string nod_name;
-	float nod_x, nod_y;
-	int nod_floor;
+class Node {
+private:
+int node_id;
+	std::string node_name;
+	float node_x, node_y;
+	int node_floor;
 	std::vector<std::pair<int, float>> neighbors;
+
+	
+	// Конструктор по умолчанию
+	Node() = default;
+
+	// Конструктор копирования
+	Node(const Node&) = default;
+
+	// Конструктор перемещения 
+	Node(Node&&) = default;
+
+	// Оператор присваивания копированием
+	Node& operator=(const Node&) = default;
+
+	// Оператор присваивания перемещением
+	Node& operator=(Node&&) = default;
+
+	// Деструктор 
+	~Node() = default;
 };
 
 
-std::unordered_map<int, Nod> nods_database;
+std::unordered_map<int, Nod> nodes_database;
